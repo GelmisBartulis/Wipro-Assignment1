4256e0d6bfcd100fa93b7d5da663245fc0813b97
GelmisBartulis <gelmis.bartulis@gmail.com>
2019-05-20 15:06:49 +0100

    Minified CS, JS, HTML and SAS

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
 // including plugins
-var gulp = require('gulp')
-, minifyHtml = require("gulp-minify-html");
+var gulp = require('gulp'),
+  minifyHtml = require("gulp-minify-html"),
+  minifyCss = require("gulp-minify-css"),
+  uglify = require("gulp-uglify"),
+  sass = require("gulp-sass"),
+  concat = require("gulp-concat");
 
 // task
 gulp.task('minify-html', done => {
-	gulp.src('./Html/*.html') // path to your files
+	gulp.src('./') // path to your files
 	.pipe(minifyHtml())
-	.pipe(gulp.dest('path/to/destination'));
+    .pipe(concat('index.html'))
+	.pipe(gulp.dest('./'));
   done();
 });
 
-
-// including plugins
-var gulp = require('gulp')
-, minifyCss = require("gulp-minify-css");
 
 // task
 gulp.task('minify-css', done => {
 	gulp.src('./css/styles.css') // path to your file
 	.pipe(minifyCss())
-	.pipe(gulp.dest('path/to/destination'));
+    .pipe(concat('styles.css'))
+	.pipe(gulp.dest('./css'));
     done();
 });
 
-var gulp = require('gulp')
-, uglify = require("gulp-uglify");
 
 // task
 gulp.task('minify-js', done => {
-	gulp.src('./JavaScript/*.js') // path to your files
+	gulp.src('./js/*.js') // path to your files
 	.pipe(uglify())
-	.pipe(gulp.dest('path/to/destination'));
+    .pipe(concat('scripts.js'))
+	.pipe(gulp.dest('./js'));
+    done();
+});
+
+
+// task
+gulp.task('compile-sass',  done => {
+	gulp.src('./sass/styles.sass') // path to your file
+	.pipe(sass())
+	.pipe(gulp.dest('./sass'));
     done();
 });
