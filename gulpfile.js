4c52850e4078c086e3554a3fd077a519c9635953
GelmisBartulis <gelmis.bartulis@gmail.com>
2019-05-20 16:47:35 +0100

    Added a bundle function to run all the functions at once

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@
 var gulp = require('gulp'),
   minifyHtml = require("gulp-minify-html"),
   minifyCss = require("gulp-minify-css"),
-  uglify = require("gulp-uglify"),
+  uglify = require('gulp-uglify-es').default,
   sass = require("gulp-sass"),
   concat = require("gulp-concat"),
-  image = require('gulp-image');
+  image = require('gulp-image'),
+  gutil   = require('gulp-util'),
+  rename = require('gulp-rename');
 
 // task
 gulp.task('minify-html', done => {
-	gulp.src('./') // path to your files
+	gulp.src('./*.html') // path to your files
 	.pipe(minifyHtml())
     .pipe(concat('index.html'))
+    .pipe(rename('min-index.html'))
 	.pipe(gulp.dest('./'));
   done();
 });
@@ -19,10 +22,10 @@ gulp.task('minify-html', done => {
 
 // task
 gulp.task('minify-css', done => {
-	gulp.src('./css/styles.css') // path to your file
+	gulp.src('./css/*.css') // path to your file
 	.pipe(minifyCss())
     .pipe(concat('styles.css'))
-	.pipe(gulp.dest('./css'));
+	.pipe(gulp.dest('./css/'));
     done();
 });
 
@@ -31,23 +34,65 @@ gulp.task('minify-css', done => {
 gulp.task('minify-js', done => {
 	gulp.src('./js/*.js') // path to your files
 	.pipe(uglify())
-    .pipe(concat('scripts.js'))
-	.pipe(gulp.dest('./js'));
+    .pipe(concat('min-scripts.js'))
+    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+	.pipe(gulp.dest('./js/'));
     done();
 });
 
 
 // task
 gulp.task('compile-sass',  done => {
-	gulp.src('./sass/styles.sass') // path to your file
+	gulp.src('./sass/*.sass') // path to your file
 	.pipe(sass())
-	.pipe(gulp.dest('./sass'));
+	.pipe(gulp.dest('./sass/'));
     done();
 });
 
 gulp.task('image', done => {
   gulp.src('./images/*')
     .pipe(image())
-    .pipe(gulp.dest('./dest'));
+    .pipe(gulp.dest('./images/'));
     done(); 
 });
+
+
+gulp.task('allTogetherNow', done => {
+	gulp.src('./') // path to your file
+    .pipe(minifyHtml())
+	.pipe(minifyCss())  // compile coffeeScript
+	.pipe(uglify())  // compile coffeeScript
+	.pipe(sass())  // compile coffeeScript
+	.pipe(image())  // compile coffeeScript
+	.pipe(concat('index.html'))
+	.pipe(concat('styles.css'))
+	.pipe(concat('styles.sass'))
+	.pipe(rename('min-index.html')) // rename into "renamed.js" (original name "one.js")
+	.pipe(gulp.dest('./'));
+    done();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
